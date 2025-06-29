e2233402bcff49b001d123c7fb70b0f61b298adf
Ceyhun Bora Işık <73286390+ceyhunbora@users.noreply.github.com>
2025-06-29 21:25:02 +0300

    Create index.jsx

---
+++ components/pages/index.jsx
@@ -0,0 +1,9 @@
+import GerledPreview from '@/components/GerledPreview';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 p-6">
+      <GerledPreview />
+    </main>
+  );
+}
